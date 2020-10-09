3f6c97fb987077f4300468002b44bfbb5d844496
Ilya <creativeartsgroupa@gmail.com>
2020-10-09 23:01:59 +0300

    ReactShop

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $5",
+        "array": [0, 5]
+    },
+    {
+        "_id": 2,
+        "name": "$5 to $10",
+        "array": [5, 10]
+    },
+    {
+        "_id": 3,
+        "name": "$10 to $20",
+        "array": [10, 20]
+    },
+    {
+        "_id": 4,
+        "name": "$20 to $30",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $30",
+        "array": [30, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "RockStars"
+    },
+    {
+        "_id": 2,
+        "name": "Steam"
+    },
+    {
+        "_id": 3,
+        "name": "Origin"
+    },
+    {
+        "_id": 4,
+        "name": "Uplay"
+    },
+    {
+        "_id": 5,
+        "name": "MineCraft"
+    },
+    {
+        "_id": 6,
+        "name": "Fortnite"
+    },
+    {
+        "_id": 7,
+        "name": "PUBG"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:"mongodb+srv://fuckyou:youfuck@cluster0.vu9qz.mongodb.net/shop?retryWrites=true&w=majority"
+}
